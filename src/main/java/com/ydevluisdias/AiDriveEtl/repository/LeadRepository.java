b2d82a7a28e589432e4b4afca5282eefdfa5a5dc
yDevLuisDias <luiscosta.official@gmail.com>
2026-07-02 18:35:24 -0300

    up repository

--- src/main/java/com/ydevluisdias/AiDriveEtl/repository/LeadRepository.java
+++ src/main/java/com/ydevluisdias/AiDriveEtl/repository/LeadRepository.java
@@ -1,9 +1,9 @@
 package com.ydevluisdias.AiDriveEtl.repository;
 
-import com.ydevluisdias.AiDriveEtl.entity.LeadEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ydevluisdias.AiDriveEtl.entity.LeadEntity;
 
 public interface LeadRepository extends JpaRepository<LeadEntity, UUID> {
+
 }
